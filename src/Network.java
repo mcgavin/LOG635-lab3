5e1b04cd6946f7351d08d7b25de1f347fe5cce42
Kuroro42 <kuroro_1337@hotmail.com>
2014-07-21 22:44:02 -0400

    Update Network.java

--- src/Network.java
+++ src/Network.java
@@ -10,23 +10,23 @@ public class Network {
 		Layer layer = new Layer(input_count, true);
 		int neural_count = layer.getNeuralCount();
 		layers.put(1, layer);
-		int layer_count = rand.nextInt(8)+3; //de 3 à 10 couches
+		int layer_count = rand.nextInt(11)+10; //de 10 à 20 couches
 		for(int i = 2; i<=layer_count; i++){
 			layer = new Layer(neural_count, false);
 			neural_count = layer.getNeuralCount();
 			layers.put(i, layer);
 		}
 	}
-	public float getResult(List input){
+	public float getResult(List<Float> input){
 		Layer layer = layers.get(1);
-		List results = layer.getValues(input);
+		List<Float> results = layer.getValues(input);
 		for(int i = 2; i<=layers.size(); i++){
 			layer = layers.get(i);
 			results = layer.getValues(results);
 		}
 		float total = 0;
-		for (Object result : results) {
-			total += (float)result;
+		for (float result : results) {
+			total += result;
 		}
 		total = total / results.size();
 		return total;
