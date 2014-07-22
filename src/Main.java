0341c8c5f3d0d9788fba6e58b72c58471d56d28d
Kuroro42 <kuroro_1337@hotmail.com>
2014-07-21 22:42:49 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
@@ -34,28 +29,30 @@ public class Main {
 		Network n = new Network(input.size());
 		System.out.println("test");
 		System.out.println(n.getResult(input));
-		*/
+		*/	
 		Network keeper = null;
 		float mean_error = 10000, kept_mean_error = 10000;
 		int count = 0;
-		
+
 		while (mean_error > 1000){
 			count++;
 			Network n = new Network(nbAttributs);
 			int error_total = 0;
 			int error_count = 0;
 			for (List<Float> list : listOfInputs) {
-				error_total += Math.abs(n.getResult(list)*10000 - listRental.get(error_count));
-				//System.out.println(n.getResult(list)*10000);
+				int result = (int)(n.getResult(list)*10000);
+				error_total += Math.abs(result - listRental.get(error_count));
 				error_count++;
 			}
 			mean_error = error_total / error_count;
+			System.out.println(mean_error);
 			if(kept_mean_error > mean_error){
 				count = 0;
 				keeper = n;
 				kept_mean_error=mean_error;
+				System.out.println(kept_mean_error);
 			}
-			if(count > 100){
+			if(count > 10){
 				break;
 			}
 		}
