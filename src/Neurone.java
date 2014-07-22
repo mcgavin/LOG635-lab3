524f3ba764b991c85b73f3af96f22820b7df2aaf
Kuroro42 <kuroro_1337@hotmail.com>
2014-07-21 22:44:18 -0400

    Update Neurone.java

--- src/Neurone.java
+++ src/Neurone.java
@@ -3,20 +3,24 @@ import java.util.List;
 
 
 public class Neurone {
-	int[] weights;
+	float[] weights;
+	float seuil;
 	int[] random_ids;
+	float w0;
 	public Neurone(int input_count, boolean isFirstLayer){
 		Random rand = new Random();
 		int link_count = 0;
 		if(isFirstLayer){
 			link_count = input_count; //prendre tout les input d'origine
 		}else{
-			link_count = rand.nextInt(11) + 10; //10 à 20 liens
+			link_count = rand.nextInt(101) + 100; //100 à 200 liens
 		}
-		weights = new int[link_count];
+		seuil = rand.nextFloat(); //de 0 à 1
+		weights = new float[link_count];
 		random_ids = new int[link_count];
+		w0 = rand.nextFloat()/2; //poids de 0 à 0.5
 		for(int i = 0; i<link_count; i++){
-			int weight = rand.nextInt(101); //poids de 0 à 100
+			float weight = (float)(rand.nextFloat()/10+0.9); //poids de 0.9 à 1
 			weights[i] = weight;
 		}
 		
@@ -26,18 +30,20 @@ public class Neurone {
 			while(selected[random_id]){
 				random_id = rand.nextInt(input_count);
 			}
-			random_ids[i] = random_id; //poids de 0 à 100
+			random_ids[i] = random_id;
 			selected[random_id] = true;
 		}
 	}
-	public float getValue(List input){
+	public float getValue(List<Float> input){
 		float sum = 0;
 		for(int i = 0; i<weights.length; i++){
 			float in = (float)input.get(random_ids[i]);
-			sum = in * weights[i]/weights.length;
+			sum = sum + (in * weights[i]);
 		}
+		float y = (sum/weights.length);
+		
 		//fonction d'activation: tangente hyperbolique
-		float y = (float) ((2.3432 / (1 + Math.exp(-0.667*sum))) - 1.1716);
+		//float y = (float) ((2.3432 / (1 + Math.exp(-0.667*sum))) - 1.1716);
 		return y;
 	}
 }
